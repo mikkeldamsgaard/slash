c891aac04f4431ed83daf7b0352a64a6e5b1b864
mikkel <mikkel@spottune.com>
2022-01-03 11:14:28 +0100

    Fixed index out of bounds when serializing

--- src/value.rs
+++ src/value.rs
@@ -270,17 +270,13 @@ impl Value {
             Value::List(l) => {
                 let mut s = String::from("");
                 l.borrow().iter().for_each(|v| s.push_str(&format!(", {}", v.to_json())));
-                if s.len() > 2 {
-                    format!("[{}]", &s[2..])
-                }
+                format!("[{}]", if s.len() > 2 { &s[2..] } else { "" })
             }
             Value::Table(t_r) => {
                 let t = t_r.borrow();
                 let mut s = String::from("");
                 t.keys().for_each(|k| s.push_str(&format!(", \"{}\": {}", Value::escape_string(k), t.get(k).unwrap().to_json())));
-                if s.len() > 2 {
-                    format!("{{{}}}", &s[2..])
-                }
+                format!("{{{}}}", if s.len() > 2 { &s[2..] } else { "" })
             }
             Value::ProcessResult(exitcode, stdout, stderr) => {
                 let pre;
